de93a11c2658c297dc4ad8906cc6f3aa1fb08ef5
uyong\uyong <uyong11@daum.net>
2023-02-05 19:37:06 +0900

    시작

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title="베토벤";
+  const img = "https://en.wikipedia.org/wiki/Ludwig_van_Beethoven#/media/File:Beethoven.jpg"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{title}</h2>
+      <img src= "https://www.bl.uk/britishlibrary/~/media/bl/global/dm%20music%201800%20to%201900/people%20pages/beethoven.jpg?crop=1&cropX=199&cropY=10&cropW=1757&cropH=989&cropcachekey=1199101757989&w=608&h=342&dispW=608&dispH=342&hash=E60BE143CD5A8CBF17206815B42EDAD8"
+        alt={title}
+      />
     </div>
   );
 }
